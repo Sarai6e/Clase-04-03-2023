764f72db97b5eb4ebfd4fbe83cbea970f36dd609
Sarai6e <1472173@senai.pe>
2023-03-04 09:35:35 -0500

    Primera y ultimo valor

---
+++ lista.py
@@ -0,0 +1,7 @@
+# Desarrolle un algoitmo que escriba un codigo de pogramacin donde me permita imprimir el primer y el ultimo valor de lalista.
+list=['red ',' blue',' green',' black',' white']
+n= list[0]
+m=list[-1]
+
+print(n)
+print(m)
